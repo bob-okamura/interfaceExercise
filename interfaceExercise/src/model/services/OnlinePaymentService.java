5473d0a5feb54e8936589adde8fd43693973854d
Roberto Okamura <rob.okamura@gmail.com>
2022-05-17 18:09:37 -0300

    Interface Exercise

---
+++ interfaceExercise/src/model/services/OnlinePaymentService.java
@@ -0,0 +1,8 @@
+package model.services;
+
+public interface OnlinePaymentService {
+	
+	double paymentFee(double amount);//taxa de pagamento
+	double interest(double amount, int months);//juros
+
+}
